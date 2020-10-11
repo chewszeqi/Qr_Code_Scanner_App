3548e1f95daeefd07e8e5b57be69ab1afdeeb0e8
Chew Sze Qi <chewszeqi@gmail.com>
2020-10-11 21:01:49 +0800

    Changes made

--- app/src/main/java/com/example/qrcodescannerapplication/ui/ui/mainactivity/MainPagerAdapter.kt
+++ app/src/main/java/com/example/qrcodescannerapplication/ui/ui/mainactivity/MainPagerAdapter.kt
@@ -13,7 +13,6 @@ class MainPagerAdapter(var fm: FragmentManager) : FragmentStatePagerAdapter(fm) 
 
     override fun getItem(position: Int): Fragment {
         return when (position){
-            0 -> QrScannerFragment2.newInstance()
             0 -> QrScannerFragment.newInstance()
             1 -> ScannedHistoryFragment.newInstance()
             2 -> ScannedHistoryFragment.newInstance()
